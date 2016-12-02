02e402859dd6ce7d60a23861f32bfe599190928d
Lester Ramos <lramo062@fiu.edu>
2016-12-02 17:54:47 -0500

    Commenting

--- main.cu
+++ main.cu
@@ -6,20 +6,20 @@
 // driver function for the program
 int main() {
 
-    // FETCH INPUT FROM USER
+    // CPU MEMORY ALLOCATION
     int *numOfVertices = (int *) malloc(sizeof(int)); // # of vertices in the graph
     int *arrayLength = (int *) malloc(sizeof(int));
 
-    printf("Please enter the number of vertices for graph: ");
+    // PROMPT USER FOR # OF VERTICES
+    printf("Enter the number of vertices for graph: ");
     scanf("%d", numOfVertices);
 
     *arrayLength = *numOfVertices * *numOfVertices;
 
+    // CPU MEMORY
     float* graph = (float *) malloc(*arrayLength * sizeof(float));
-    float* result = (float *) malloc(sizeof(float) * *numOfVertices);
-    
-    
-
+    float* result = (float *) malloc(*numOfVertices * sizeof(float));
+        
     // FUNCTION CALLS (CPU)
     createGraph(graph, *arrayLength); // Generate the graph & store in array
     printGraph(graph, *arrayLength); // Print the array
@@ -27,26 +27,29 @@ int main() {
     printSolution(result, *numOfVertices);
     
 
-    // FUNCTION CALLS (GPU)
+    // GPU MEMORY ALLOCATION
     int *d_numOfVertices, *d_arrayLength;
-    float* d_graph; // GRAPH DEVICE COPY
+    float* d_graph, d_result;
 
     cudaMalloc((void **) &d_numOfVertices, sizeof(int));
     cudaMalloc((void **) &d_arrayLength, sizeof(int));
-    cudaMalloc((void **) &d_graph, (*arrayLength * sizeof(float))); // allocate mem
+    cudaMalloc((void **) &d_graph, (*arrayLength * sizeof(float)));
+    cudaMalloc((void **) &d_result, (*numOfVertices * sizeof(float)));
 
+    // COPY CPU MEM --> GPU MEM
     cudaMemcpy(d_numOfVertices, &numOfVertices, (*arrayLength * sizeof(float)), cudaMemcpyHostToDevice);
     cudaMemcpy(d_arrayLength, &arrayLength, (*arrayLength * sizeof(float)), cudaMemcpyHostToDevice);
     cudaMemcpy(d_graph, &graph, (*arrayLength * sizeof(float)), cudaMemcpyHostToDevice);
     
     
-    // GPU Dijkstra Call
 
+    // FREE GPU MEM
     cudaFree(d_graph);
     cudaFree(d_numOfVertices);
     cudaFree(d_arrayLength);
 
 
+    // FREE CPU MEM
     free(numOfVertices);
     free(arrayLength);
     free(graph);
