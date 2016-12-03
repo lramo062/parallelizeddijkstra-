ef8d74b8c7fd323154299e5e0dc214b0bc562c5d
Lester Ramos <lramo062@fiu.edu>
2016-12-03 16:46:11 -0500

    Trying to parallize the calculations

--- gpu.cu
+++ gpu.cu
@@ -3,40 +3,47 @@
 // ================== Function: dijkstra ====================
 // Funtion that implements Dijkstra's single source shortest path algorithm
 // for a graph represented using array representation
-__global__ void gpu_dijkstra(float *graph, int src, bool *sptSet, int V, float *result)
-{
+__global__ void gpu_dijkstra(float *graph, bool *sptSet, float *result) {
 
-     int *min_index;
-     
      // Initialize all distances as INFINITE and stpSet[] as false
-     for (int i = 0; i < V; i++)
-        result[i] = INT_MAX, sptSet[i] = false;
-  
-     // Distance of source vertex from itself is always 0
-     result[src] = 0;
-  
-     // Find shortest path for all vertices
-     for (int count = 0; count < V-1; count++)
-     {
-       // Pick the minimum distance vertex from the set of vertices not
-       // yet processed. u is always equal to src in first iteration.
-       float min = INT_MAX;
-  
-       for (int v = 0; v < V; v++)
-         if (sptSet[v] == false && result[v] <= min)
-           min = result[v], *min_index = v;
+     int index = threadIdx.x + blockIdx.x * blockDim.x;
+    
+
+     if(index == ((blockDim.x * blockIdx.x) + blockIdx.x))
+       result[index] = 0; // distance to itself is always 0
+
+     else result[index] = INT_MAX; // else initialize infinite
+     
+     sptSet[index] = false; // is shortest
+     
+
+     /* COMMON CODE ENDS */
+     __syncthreads();
+
+
+     
+
+     // HAVENT IMPLEMENTED IN PARALLEL
+     int min, min_index;
+     
+     for (int count = 0; count < blockDim.x-1; count++) {
+       if (sptSet[index] == false && result[index] <= min)
+           min = result[index], min_index = index;
+
        // Mark the picked vertex as processed
-       sptSet[*min_index] = true;
+       sptSet[min_index] = true;
   
        // Update dist value of the adjacent vertices of the picked vertex.
-       for (int v = 0; v < V; v++) {
-  
-         // Update dist[v] only if is not in sptSet, there is an edge from 
+       for (int v = 0; v < blockDim.x; v++) {
+           // Update dist[v] only if is not in sptSet, there is an edge from 
          // u to v, and total weight of path from src to  v through u is 
          // smaller than current value of dist[v]
-           if (!sptSet[v] && graph[(*min_index * V) + v] && result[*min_index] != INT_MAX
-               && result[*min_index]+graph[(*min_index * V) + v] < result[v])
-               result[v] = result[*min_index] + graph[(*min_index * V) + v];
+         if (!sptSet[v] && graph[(min_index*blockDim.x) + index] && result[min_index] != INT_MAX
+             && result[min_index]+graph[(min_index*blockDim.x) + index] < result[index])
+           result[index] = result[min_index] + graph[(min_index*blockDim.x) + index];
+
+         __syncthreads();
        }
+       __syncthreads();
      }
 }
--- main.cu
+++ main.cu
@@ -6,7 +6,6 @@
 // driver function for the program
 int main() {
 
-    // CPU MEMORY ALLOCATION
     int *numOfVertices = (int *) malloc(sizeof(int)); // # of vertices in the graph
     int *arrayLength = (int *) malloc(sizeof(int));
 
@@ -16,46 +15,64 @@ int main() {
 
     *arrayLength = *numOfVertices * *numOfVertices;
 
-    // CPU MEMORY
+
+
+
+
+    
+    // ALLOCATE CPU MEMORY
     float* graph = (float *) malloc(*arrayLength * sizeof(float));
     float* result = (float *) malloc(*arrayLength * sizeof(float));
         
     // FUNCTION CALLS (CPU)
     createGraph(graph, *arrayLength); // Generate the graph & store in array
     printGraph(graph, *arrayLength); // Print the array
-    dijkstra(graph, 1, *numOfVertices, result); 
+
+    dijkstra(graph, 0, *numOfVertices, result); 
     printSolution(result, *numOfVertices);
     
+    dijkstra(graph, 1, *numOfVertices, result); 
+    printSolution(result, *numOfVertices);
 
-    // GPU MEMORY ALLOCATION
-    int *d_numOfVertices, *d_arrayLength;
+    dijkstra(graph, 2, *numOfVertices, result); 
+    printSolution(result, *numOfVertices);
+
+
+
+
+
+
+    
+    // ALLOCATE GPU MEMORY 
     float *d_graph, *d_result;
     bool *d_sptSet;
 
-    cudaMalloc((void **) &d_numOfVertices, sizeof(int));
-    cudaMalloc((void **) &d_arrayLength, sizeof(int));
-    cudaMalloc((void **) &d_sptSet, (*numOfVertices * sizeof(bool)));
+    cudaMalloc((void **) &d_sptSet, (*arrayLength * sizeof(bool)));
     cudaMalloc((void **) &d_graph, (*arrayLength * sizeof(float)));
-    cudaMalloc((void **) &d_result, (*numOfVertices * sizeof(float)));
-
+    cudaMalloc((void **) &d_result, (*arrayLength * sizeof(float)));
+    
     // COPY CPU MEM --> GPU MEM
-    cudaMemcpy(d_numOfVertices, &numOfVertices, (*arrayLength * sizeof(float)), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_arrayLength, &arrayLength, (*arrayLength * sizeof(float)), cudaMemcpyHostToDevice);
     cudaMemcpy(d_graph, &graph, (*arrayLength * sizeof(float)), cudaMemcpyHostToDevice);
     
-   
-    //gpu_dijkstra<<<1,1>>>(d_graph,index,d_sptSet,*numOfVertices,d_result);
-      
+    gpu_dijkstra<<<*numOfVertices,*numOfVertices>>>(d_graph, d_sptSet,d_result);
       
     // COPY GPU MEM GPU --> CPU
-    //cudaMemcpy(result, &d_result, (*arrayLength * sizeof(float)), cudaMemcpyDeviceToHost); 
+    cudaMemcpy(result, d_result, (*arrayLength * sizeof(float)), cudaMemcpyDeviceToHost); 
+    printGraph(result, *arrayLength);
+
+
+
+
+
+
+
+
+
     
     // FREE GPU MEM
     cudaFree(d_graph);
-    cudaFree(d_numOfVertices);
-    cudaFree(d_arrayLength);
-
-
+    cudaFree(d_result);
+ 
     // FREE CPU MEM
     free(numOfVertices);
     free(arrayLength);
